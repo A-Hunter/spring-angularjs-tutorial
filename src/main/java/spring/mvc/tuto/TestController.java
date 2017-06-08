b0f966a4a8082d1153d6a5ce801342296eed09ed
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-08 04:50:08 +0100

    Spring MVC Basic configuration files

---
+++ src/main/java/spring/mvc/tuto/TestController.java
@@ -0,0 +1,15 @@
+package spring.mvc.tuto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Ghazi Naceur on 08/06/2017.
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "view";
+    }
+}
