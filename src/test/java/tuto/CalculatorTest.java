a897ef30ae57ec35f9b69f64107f08f85a9e4aff
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-08 15:36:45 +0100

    Testing with Mockito

--- src/test/java/tuto/CalculatorTest.java
+++ src/test/java/tuto/CalculatorTest.java
@@ -35,6 +35,12 @@ public class CalculatorTest {
 //        int expected = 4;
 //        int actual = calculator.abs(-4);
 //        assertEquals(expected,actual);
+        /**
+         * By default, Mockito when it will instantiate our object Calculator
+         * the abs(int arg) will return 0 in all cases, so we need to call :
+         *          when(calculator.abs(-arg)).thenReturn(arg);
+         *      in order to make the abs(int arg) return the right value, not 0
+         */
         when(calculator.abs(-5)).thenReturn(5);
         assertEquals(5, calculator.abs(-5));
     }
